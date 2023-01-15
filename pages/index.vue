054f91d3f587eb65f03db9196d0a782178710886
TEKnas29 <25704089+TEKnas29@users.noreply.github.com>
2023-01-15 19:53:59 +0530

    bracket bug fixed

--- pages/index.vue
+++ pages/index.vue
@@ -12,14 +12,21 @@ export default {
       let regex = new RegExp("[A-Z][a-z]?\\d*|\\((?:[^()]*(?:\\(.*\\))?[^()]*)+\\)\\d+", 'gm') // https://stackoverflow.com/questions/23602175/regex-for-parsing-chemical-formulas
       let reg1 = new RegExp("\'\'",'gm') //to remove double quates
       let reg2 = new RegExp("\'(.*?)\'",'gm') //values between quotes
-      let reg3 = new RegExp('\\\'(Th|A|It|Be|Wh|De|So)\\\'', 'gm') //exception list 
+      let reg3 = new RegExp('\\\'(Th|A|It|Be|Wh|De|So|Fu|Ha|(\. [A-z][A-z]))\\\'', 'gm') //exception list 
       let reg4 = new RegExp('tert', 'gm') 
       let reg5 = new RegExp('(SN2|sp3|\'SN2\')', 'gm') 
+      let reg6 = new RegExp('\\b[0-9]\\b', 'gm') 
+      let splchars = new RegExp('(α|β)','gm')
       // TODO ISL part
       let inp = this.inp
 
       let chk1 = inp.replace(regex,function (x) {
-        return `\'${x}\'`
+        let nreg = new RegExp("^((Ac|Ag|Al|Am|Ar|As|At|Au|B|Ba|Be|Bh|Bi|Bk|Br|C|Ca|Cd|Ce|Cf|Cl|Cm|Co|Cr|Cs|Cu|Ds|Db|Dy|Er|Es|Eu|F|Fe|Fm|Fr|Ga|Gd|Ge|H|He|Hf|Hg|Ho|Hs|I|In|Ir|K|Kr|La|Li|Lr|Lu|Md|Mg|Mn|Mo|Mt|N|Na|Nb|Nd|Ne|Ni|No|Np|O|Os|P|Pa|Pb|Pd|Pm|Po|Pr|Pt|Pu|Ra|Rb|Re|Rf|Rg|Rh|Rn|Ru|S|Sb|Sc|Se|Sg|Si|Sm|Sn|Sr|Ta|Tb|Tc|Te|Th|Ti|Tl|Tm|U|V|W|Xe|Y|Yb|Zn|Zr)\\d*)+$","gm") 
+        if(x.match(nreg)){
+          return `\'${x}\'`
+        }else{
+          return `${x}`
+        }
       })
       let chk2 = chk1.replace(reg1,'')
       let chk3 = chk2.replace(reg3,function (s) {
@@ -41,12 +48,30 @@ export default {
         let  z = y.replace(nreg,'')
         return `@chem_${z};`
       })
-      this.opEng = `<text ref=EP_text1>${chk6}</text>`
+      let chk6a = chk6.replace(reg6,function (t) {
+          return `@userf.disp('${t}');`
+      })
+      let chk6b = chk6a.replace(splchars,function (s) {
+        switch (s) {
+          case 'α':
+              return `@alpha;`
+            break;
+          case 'β':
+              return `@beta;`
+            break;
+        
+          default:
+            break;
+        }
+        
+      })
+      this.opEng = `<text ref=EP_text1>${chk6b}</text>`
       // ENG  end
       //ISL start
         let chk7 = chk3.match(reg2)
         let isl = `<!-- ########CHEMSYMB######### -->\n`;
         let isl2 = `<!-- ########ANSPRO######### -->\n`;
+        let isl1_temp = ``;
         let isl2_temp = ``;
         let nreg3 = new RegExp("1\,\,","g")
         let nreg4 = new RegExp("\,\}","g")
@@ -54,14 +79,16 @@ export default {
         if (chk7) {
         chk7.forEach((m)=>{
           let nreg = new RegExp('[0-9]','gm')
-          let nreg1 = new RegExp("\'","g")
+          let nreg1 = new RegExp("\'","gm")
           let n = m.replace(nreg, function (p) {
               let q = p.replace(nreg1,'')
               return `<sub>${q}</sub>`
             })
           let i = m.replace(nreg1,'')
-            isl += `<var name=chem_${i} value="<math><font face=chemsymb>${n}</font></math>">\n`
-          
+
+            isl1_temp += `<var name=chem_${i} value="<math><font face=chemsymb>${n}</font></math>">\n`
+     
+            
         })
         chk7.forEach((n)=>{
           let nreg = new RegExp('[0-9]','gm')
@@ -85,7 +112,13 @@ export default {
         let m = l.replace(nreg4,'\}')
         let n = m.replace('\,)','\}')
         let o = n.replace('(','\{')
-          isl2 += o
+        isl2 += o;
+        isl = isl + isl1_temp.split("\n")
+                    .filter((item, i, allItems) => {
+                      return i === allItems.indexOf(item);
+                    })
+                    .join("\n");
+        // console.log(isl1_temp);
         this.opisl = isl + isl2
       }
       //ISL end
@@ -113,12 +146,13 @@ export default {
     <div class="op">
       <div class="op1">
         <h3>ISL:</h3>
-        <!-- <textarea name="islOp" id="IslOp" cols="100" rows="10" class="opArea" readonly >{{ opisl }}</textarea> -->
+        <i>Note: <b>'</b> is left there in output</i><br>
+        <i>Note: <b>-/+</b> is not included</i>
         <pre class="opArea"><code>{{ opisl }}</code></pre>
       </div>
       <div class="op2">
         <h3>English:</h3>
-        <!-- <textarea name="engOp" id="IslOp" cols="100" rows="10" class="opArea" readonly ></textarea> -->
+        <i>Note:Check before direct use still experimental</i>
         <pre class="opArea"><code>{{ opEng }}</code></pre>
       </div>
     </div>
